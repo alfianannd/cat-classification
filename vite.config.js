cfa9226d8c3669b618577ec4447471a1059067a8
alfianannd <anndalfian@gmail.com>
2025-08-20 19:02:49 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["lucide-react", "axios"], // Add lucide-react here
-    },
-  },
   plugins: [tailwindcss()],
 });
